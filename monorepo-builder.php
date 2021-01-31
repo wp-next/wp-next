a7570a91a3717e89c222b3a176f22cfa15fff04f
Kim Larsson <kim@bravisismo.se>
2021-01-31 20:41:01 +0100

    prepare release

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
-use Symplify\MonorepoBuilder\ValueObject\Option;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
@@ -13,14 +11,18 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesRele
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
+use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
-    // for "merge" command
-    $parameters->set(Option::DATA_TO_APPEND, [
-        ComposerJsonSection::REQUIRE_DEV => [
-            'phpunit/phpunit' => '^9.5',
+    $parameters->set(Option::DATA_TO_REMOVE, [
+        'require' => [
+            # remove these to merge of packages' composer.json
+            'tracy/tracy' => '*',
+            'phpunit/phpunit' => '*',
         ],
+        'minimum-stability' => 'dev',
+        'prefer-stable' => true,
     ]);
 
     $services = $containerConfigurator->services();
